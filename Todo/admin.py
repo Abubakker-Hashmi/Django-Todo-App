9eab3641c55ea4398ad24c2d05861fac072a077b
Abubakker-Hashmi <abubakkerhashmi123@gmail.com>
2023-07-04 18:34:48 +0500

    first commit

---
+++ Todo/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Todo
+
+class TodoAdmn(admin.ModelAdmin):
+    readonly_fields=('created',)
+# Register your models here.
+admin.site.register(Todo,TodoAdmn)
